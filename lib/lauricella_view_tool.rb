632810e0464c42e2bdb1014778c84b1f483e8903
Samuel Lauricella <samuellauricella@Samuels-MacBook-Pro.local>
2017-10-16 17:36:03 -0400

    updated gitignore and readme

--- lib/lauricella_view_tool.rb
+++ lib/lauricella_view_tool.rb
@@ -3,10 +3,6 @@ require "lauricella_view_tool/renderer"
 
 module LauricellaViewTool
 
-  class Renderer
-    def self.copyright name,msg
-      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
-    end
-  end
+
   # Your code goes here...
 end
